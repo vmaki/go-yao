4d41013fb967b1c196d37196f74e69fcb298d3ce
maki <lindon446@gmail.com>
2023-02-21 10:13:42 +0800

    读取环境变量

--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"flag"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cast"
 	"go-yao/boot"
 	"go-yao/pkg/global"
 )
 
-func main() {
-	boot.SetupConfig("")
+func init() {
+	flag.StringVar(&global.Env, "env", "", "加载 settings.yml，如 --env=dev 加载的是 settings.dev.yml")
+	flag.Parse()
 
+	boot.SetupConfig(global.Env)
+}
+
+func main() {
 	r := gin.New()
 	boot.SetupRoute(r)
 
--- pkg/global/global.go
+++ pkg/global/global.go
@@ -3,5 +3,6 @@ package global
 import "go-yao/config"
 
 var (
+	Env  string
 	Conf = new(config.AppConfig)
 )
