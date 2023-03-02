b882b1c803154b7a912f0fc0d7a8319ee988d289
maki <lindon446@gmail.com>
2023-03-02 23:26:38 +0800

    dd

--- go.sh
+++ go.sh
@@ -4,18 +4,18 @@ name=go-yao-docker
 # 停止该镜像正在运行的Docker容器
 # shellcheck disable=SC2143
 if [ -n "$(docker ps | grep $name)" ]; then
-	echo "存在正在运行的$name容器, 正在使其停止运行..."
+  echo "存在" "$name" "容器, 正在使其停止运行..."
 	docker stop $name
-	echo "$name容器, 已停止运行"
+	echo "$name" "容器已停止运行"
 fi
 
 # 删除该镜像的Docker容器
 # line=`docker ps -a | grep $name`
 # shellcheck disable=SC2143
 if [ -n "$(docker ps -a | grep $name)" ]; then
-	echo "存在$name容器, 对其进行删除..."
+  echo "存在" "$name" "容器, 对其进行删除..."
 	docker rm $name
-	echo "$name容器, 已被删除"
+	echo "$name" "容器已被删除"
 fi
 
 docker-compose build
